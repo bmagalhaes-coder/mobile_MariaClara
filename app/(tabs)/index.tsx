c2120f1215a29db70981e6acb8e126e6db813358
bmagalhaes-coder <bmagalhaes@acad.ifma.edu.br>
2026-06-18 17:49:55 -0300

    a

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,59 +1,83 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#06173d', dark: '#1f0d47' }}
+      headerBackgroundColor={{ light: '#006633', dark: '#004d26' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={{
+            uri: 'https://images.even3.com/zh7vbB-Kca5BMNnS71YK1GD9qOU=/1100x440/smart/https://static.even3.com/banner/BANNEREVEN3.a251c4d5270742d4a04b.png',
+          }}
+          style={styles.headerImage}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Maria Clara Barros Magalhães 😀</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">📚 Biblioteca IFMA</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Informática 2024</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Objetivo</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
 
-        <ThemedText>
-          {`Virar professora de matemática.`}
+      <ThemedText style={styles.description}>
+        Confira alguns livros disponíveis na Biblioteca IFMA.
+      </ThemedText>
+
+      {/* Livro 1 */}
+      <ThemedView style={styles.card}>
+        <ThemedText type="subtitle">
+          📖 As Crônicas de Nárnia
         </ThemedText>
+
+        <ThemedText style={styles.author}>
+          Autor: C. S. Lewis
+        </ThemedText>
+
+        <Image
+          source={{
+            uri: 'https://harpercollins.com.br/cdn/shop/files/9786560051959_f7c24963-6dd6-41c9-bdba-739f5e4cbec5.jpg?v=1781712870&width=350',
+          }}
+          style={styles.bookImage}
+        />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle"> </ThemedText>
+
+      {/* Livro 2 */}
+      <ThemedView style={styles.card}>
+        <ThemedText type="subtitle">
+          📖 Livro do Desassossego
+        </ThemedText>
+
+        <ThemedText style={styles.author}>
+          Autor: Fernando Pessoa
+        </ThemedText>
+
+        <Image
+          source={{
+            uri: 'https://cdl-static.s3-sa-east-1.amazonaws.com/covers/gg/9786559215621/livro-do-desassossego-edicao-revista-e-atualizada.jpg',
+          }}
+          style={styles.bookImage}
+        />
+      </ThemedView>
+
+      {/* Livro 3 */}
+      <ThemedView style={styles.card}>
+        <ThemedText type="subtitle">
+          📖 A Hora da Estrela
+        </ThemedText>
+
+        <ThemedText style={styles.author}>
+          Autor: Clarice Lispector
+        </ThemedText>
+
+        <Image
+          source={{
+            uri: 'https://m.media-amazon.com/images/I/61TaHURu27L._SL1000_.jpg',
+          }}
+          style={styles.bookImage}
+        />
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -61,19 +85,48 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    gap: 8,
+    alignItems: 'center',
+    marginBottom: 20,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  description: {
+    textAlign: 'center',
+    marginBottom: 20,
+    fontSize: 16,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  card: {
+    backgroundColor: '#F2F8F2',
+    padding: 15,
+    marginBottom: 20,
+    borderRadius: 15,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+
+    elevation: 4,
+  },
+
+  author: {
+    marginTop: 5,
+    marginBottom: 10,
+    fontSize: 15,
+  },
+
+  bookImage: {
+    width: 180,
+    height: 270,
+    alignSelf: 'center',
+    borderRadius: 10,
+  },
+
+  headerImage: {
+    width: '100%',
+    height: '100%',
   },
 });
