2ad52ba193e5c2f9a3d59dc8c398f8911d6cff2f
joaosetubal-commits <joaosetubal@acad.ifma.edu.br>
2026-06-19 15:04:11 -0300

    ws

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -24,7 +24,7 @@ export default function HomeScreen() {
         <ThemedText type="title" style={styles.logoText}>
           Biblioteca IFMA - Timon📚
         </ThemedText>
-      </ThemedView>
+      </ThemedView>d
 
       {/* Perfil */}
       <ThemedView style={styles.profileCard}>
